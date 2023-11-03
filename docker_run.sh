818779239d6295ba3414d49589490578c8137844
Harsh Parashar <parasharharsh16@gmail.com>
2023-11-03 23:53:29 +0530

    Assignment-4 added bash file

--- docker_run.sh
+++ docker_run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-pytest
-python code_exp.py 5 0.2 0.1 [svm,dt] config.json
+
+docker build -t digits:v1 -f docker/Dockerfile .
+rm models/*
+echo "Models before execution"
+ls -lh models
+echo "Run docker image"
+docker run -v ./models:/digits/models digits:v1
+echo "Models after execution"
+ls -lh models
