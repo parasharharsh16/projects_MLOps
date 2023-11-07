a241581fd77056f06acdb599905671c2bb9f1694
Harsh Parashar <parasharharsh16@gmail.com>
2023-11-07 21:04:26 +0530

    Expose the flask app out of docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 pytest
-python code_exp.py 5 0.2 0.1 [svm,dt] config.json
+#python code_exp.py 5 0.2 0.1 [svm,dt] config.json
+#Export FLASK_APP = main.py
+export FLASK_APP=api/main
+#flask run
+flask run --host=0.0.0.0
