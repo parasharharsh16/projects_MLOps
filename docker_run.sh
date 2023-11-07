40a26a8f3121dcf956e1a2eb94d0a0f8b312e8b9
Harsh Parashar <parasharharsh16@gmail.com>
2023-11-07 22:55:28 +0530

    Removed intractive

--- docker_run.sh
+++ docker_run.sh
@@ -8,4 +8,4 @@ docker build -t digits:v1 -f docker/Dockerfile .
 # docker run -v ./models:/digits/models digits:v1
 # echo "Models after execution"
 # ls -lh models
-docker run -it -p 5000:5000 digits:v1
+docker run -p 5000:5000 digits:v1
