c406f57e460629f66fffce9807ea6bfd6720a8d8
Harsh Parashar <parasharharsh16@gmail.com>
2023-11-07 22:43:03 +0530

    Assignment

--- docker_run.sh
+++ docker_run.sh
@@ -2,9 +2,10 @@
 
 docker build -t digits:v1 -f docker/Dockerfile .
 rm models/*
-echo "Models before execution"
-ls -lh models
-echo "Run docker image"
-docker run -v ./models:/digits/models digits:v1
-echo "Models after execution"
-ls -lh models
+# echo "Models before execution"
+# ls -lh models
+# echo "Run docker image"
+# docker run -v ./models:/digits/models digits:v1
+# echo "Models after execution"
+# ls -lh models
+docker run -it -p 5000:5000 digits:v1
