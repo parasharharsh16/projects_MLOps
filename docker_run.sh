a9c8a3a85b5499082988cfbede95a12c813da1d6
Harsh Parashar <parasharharsh16@gmail.com>
2023-11-07 23:00:23 +0530

    Removed flask expose

--- docker_run.sh
+++ docker_run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 docker build -t digits:v1 -f docker/Dockerfile .
-#rm models/*
-# echo "Models before execution"
-# ls -lh models
-# echo "Run docker image"
-# docker run -v ./models:/digits/models digits:v1
-# echo "Models after execution"
-# ls -lh models
-docker run -p 5000:5000 digits:v1
+rm models/*
+echo "Models before execution"
+ls -lh models
+echo "Run docker image"
+docker run -v ./models:/digits/models digits:v1
+echo "Models after execution"
+ls -lh models
+#docker run -p 5000:5000 digits:v1
