34464f325db56041fc82a6957ff8158c3d2b9d95
Harsh Parashar <parasharharsh16@gmail.com>
2023-11-14 21:51:27 +0530

    Assignment 5 publish

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pytest
-python code_exp.py 5 0.2 0.1 [svm,dt] config.json
-# export FLASK_APP=api/main
+#python code_exp.py 5 0.2 0.1 [svm,dt] config.json
+export FLASK_APP=api/main
 # #flask run
-# flask run --host=0.0.0.0
+flask run --host=0.0.0.0
